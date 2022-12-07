302b864b25ca37e27729d052756b3c153706c445
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-12-07 17:46:20 +0300

    add seed data for the api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,154 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "🌱 Seeding spices..."
+
+Architectural.create!([
+  {
+    title: "Mombasa Apartment",
+    image_url: "https://media-exp1.licdn.com/dms/image/C4D22AQEi8I7YADSY7g/feedshare-shrink_800/0/1628889367636?e=1673481600&v=beta&t=6C38J8nz2nnwxjthvFY7G5WpsxFVsj5-Sy8FUpo6sEE",
+    content: "The project lays on a 35
+    M
+    *
+    40M plot of land in
+    Mombasa town.
+     The client requested
+    for a simple and modern
+    design with the key features
+    including:- -one bedroom apartments
+    -Studio Apartments
+    -Commercial plotss on the ground floor.",
+    
+    
+    budget: "150,000,000"
+  },
+  {
+    title: "Eldoret Maissonette",
+    image_url: "https://media-exp1.licdn.com/dms/image/C4D22AQFNbxVaMqGGTw/feedshare-shrink_2048_1536/0/1616437703964?e=1673481600&v=beta&t=NgKicwY0IMTFKgFd6dOHg36d26NbA53RTVKJJz3ASck",
+    content: "The project lays on a 35
+    M
+    *
+    40M plot of land in
+    Eldoret
+    ,Ngeria
+    ,Blossom
+    Estate. The client requested
+    for a simple and modern
+    design with the key features
+    including:- -An open plan living room
+    opening up to a beauƟful
+    pergola on the backyard -An double door garage
+    fiƫng 2 cars -4 bedrooms and 3 baths -A master bedroom with
+    a
+    ,master bath, a walk in closet
+    and a private rooŌop garden
+    with a jacuzzi.",
+    
+    budget: "10,000,000"
+  },
+  {
+    title: "Blossom Maissonette",
+    image_url: "https://media-exp1.licdn.com/dms/image/C4D22AQEIIVxSdxJ9rw/feedshare-shrink_2048_1536/0/1611449931057?e=1673481600&v=beta&t=LFsxzm69UUPlq4Eb7O7z-sZzWenCql11U4xa6eI_p1E",
+    content: "The project lays on a 35
+    M
+    *
+    40M plot of land in
+    Eldoret
+    ,Ngeria
+    ,Blossom
+    Estate. The client requested
+    for a simple and modern
+    design with the key features
+    including:- -An open plan living room
+    opening up to a beauƟful
+    pergola on the backyard -An double door garage
+    fiƫng 2 cars -4 bedrooms and 3 baths -A master bedroom with
+    a
+    ,master bath, a walk in closet
+    and a private rooŌop garden
+    with a jacuzzi.",
+    
+    budget: "10,000,000"
+  },
+
+  {
+    title: "Steel Manufacturers",
+    image_url: "https://media-exp1.licdn.com/dms/image/C4D22AQEU7rD4IX2t1w/feedshare-shrink_2048_1536/0/1632696418446?e=1673481600&v=beta&t=5TbYdU09gSCHklFjivcW7z1GQIHmke37T5ew3czkUt0",
+    content: "The project lays on a 60
+    M
+    *
+    100M plot of land in
+    Bamburi town.
+     The client requested
+    for a modern warehouse design that could reflect on their brand. It consists of features such as:- 
+        -Executive Offices
+        -A warehouse storage area
+        -A car parking",
+    
+    
+    budget: "75,000,000"
+  },
+  {
+    title: "Ainabkoi Farmhouse",
+    image_url: "https://media-exp1.licdn.com/dms/image/C5622AQFAiE3E33NTsg/feedshare-shrink_1280/0/1588131767008?e=1673481600&v=beta&t=ovqd0bVicAMDmxM1pJzsxmoU7GqtvofckrwzUsCoB9k",
+    content: "*The project is a four bedroom bungalow siƫng on a 3
+    acre piece of land in Ainabkoi, Uasin-Gishu county.
+    *The key features for the project include:
+    -An open Kitchen plan.
+    -3 En-suite bedrooms.
+    -A spacious living room and dining room
+    opening up to a beauƟful pergola.
+    - A master Bedroom with a master bath
+    and a wakl-in closet.",
+    
+    budget: "7,500,000"
+  },
+  {
+    title: "Nyali Apartment",
+    image_url: "https://media-exp1.licdn.com/dms/image/C4D22AQHdUk33UryFhg/feedshare-shrink_1280/0/1633280582547?e=1673481600&v=beta&t=YlFN0ZBEcD-bftXzQBHj6xxIFRoy96yEsKR0P1luEU4",
+    content: "The project is a proposal for an apartment that sits  on a 35M*40M plot of land in Nyali town.
+     The client requested
+    for a unique design that would stand-out.
+    I consists of featueres such as:- 
+        -Two bedroom apartments.
+        -A green and playful facade
+        -A penthouse with a roof garden
+        -A car parking on the ground floor
+    ",
+    
+    
+    budget: "120,000,000"
+  }])
+
+  Webdev.create!([
+    {
+    title: "SaiBnb",
+    image_url: "https://images.pexels.com/photos/2440471/pexels-photo-2440471.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    web_url: "https://Masai11.github.io/SAIBNB-Phase_1/"
+    },
+    {
+    title: "Cy-Comme",
+    image_url: "https://images.pexels.com/photos/1020052/pexels-photo-1020052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    web_url: "https://legendary-licorice-cc668e.netlify.app/"
+    },
+    {
+    title: "SaiDar",
+    image_url: "https://images.pexels.com/photos/4919741/pexels-photo-4919741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    web_url: "https://Masai11.github.io/Week-3--Dairy-Business/"
+    },
+    {
+        title: "Agriplus",
+        image_url: "https://images.unsplash.com/photo-1464226184884-fa280b87c399?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        web_url: "https://Masai11.github.io/Week-2-IP-AgriPlus-Website/"
+    },
+    {
+        title: "Vallies-estate",
+        image_url: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        web_url: "https://Masai11.github.io/week1-project-Vallies-estate-/"
+    },
+    {
+        title: "BetterYou",
+        image_url: "https://images.pexels.com/photos/5699451/pexels-photo-5699451.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        web_url: "https://iamkibeh.github.io/Betteryou/"
+    }
+])
+
+puts "✅ Done seeding!"
+  
