507c75bd3de4b72b972a7b7a124e6f89e85b23a3
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-12-09 19:00:54 +0300

    add a new form component

--- client/src/components/Achitectural.js
+++ client/src/components/Achitectural.js
@@ -2,11 +2,31 @@ import React from 'react'
 import './architectural.css'
 
 import {useState,useEffect} from 'react'
+import NewArchitectureProjectForm from './NewArchitectureProjectForm'
+
+const ReadMore = ({ children }) => {
+  const text = children
+
+  const [isReadMore, setIsReadMore] = useState(true)
+
+  const toggleReadMore = ()=>{
+    setIsReadMore(!isReadMore)
+  }
+  return(
+    <p className='text'>
+      {isReadMore ? text.slice(0,150) : text}
+      <span onClick={toggleReadMore} className='read-or-hide'>
+        {isReadMore ? "...Read More" : "Show Less"}
+      </span>
+    </p>
+  )
+}
 
 
 
 const Achitectural = () => {
   const [projects, setProjects] = useState([]);
+  const [formVisible, setFormVisible] = useState(false)
 
   useEffect(()=>{
     fetch("/architecturals")
@@ -14,9 +34,14 @@ const Achitectural = () => {
     .then((projects) => setProjects(projects) )
   })
 
+
   return (
     <>
       <h1 className='head'>Architecture Projects</h1>
+      <div className='btn'>    
+          <button onClick={()=>{setFormVisible(!formVisible)}} className='form-btn'>Add New</button>
+      </div>
+      {formVisible ? <NewArchitectureProjectForm/> : null}
     <div className='arch-container'>
       {projects.map((project) =>{
         return (
@@ -24,6 +49,14 @@ const Achitectural = () => {
           <div className ='arch-card'> 
             <img src={project.image_url} className='card-img'/>
             <h2>{project.title}</h2>
+            <h4>
+
+              <ReadMore>
+                {project.content}
+                
+              </ReadMore>
+            </h4>
+            <h4>ksh. {project.budget}M</h4>
             <button>View</button>
             
           </div>
@@ -35,3 +68,4 @@ const Achitectural = () => {
 }
 
 export default Achitectural
+
